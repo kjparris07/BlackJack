97b166c7acba2846c0970d958810e01732bab8b0
kyra <kjparris07@gmail.com>
2022-11-16 18:39:18 -0500

    implmented Card and Deck classes

---
+++ src/Card.java
@@ -0,0 +1,106 @@
+public class Card {
+    String name, face;
+    private int value, suit;
+
+    public Card(String name, String face, int value, int suit) {
+        this.name = name;
+        this.face = face;
+        this.value = value;
+        if (suit > -1 && suit < 4) {
+            this.suit = suit;
+        } else {
+            this.suit = 0;
+        }
+    }
+
+    public String getFace() {
+        return face;
+    }
+
+    public void setFace(String face) {
+        this.face = face;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public void setSuit(int suit) {
+        if (suit > -1 && suit < 4) {
+            this.suit = suit;
+        }
+    }
+
+    public String getCard() {
+        String icon;
+        if (suit == 0) {
+            icon = "\u2663"; // clubs
+        } else if (suit == 1) {
+            icon = "\u2666"; // diamonds
+        } else if (suit == 2) {
+            icon = "\u2665"; // hearts
+        } else if (suit == 3) {
+            icon = "\u2660"; // spades
+        } else {
+            icon = "X";
+        }
+        if (this.value == 10) {
+            return " _____ \n|" + face +"   |\n|  " + icon + "  |\n|_____|";
+        } else {
+            return " _____ \n|" + face +"    |\n|  " + icon + "  |\n|_____|";
+        }
+    }
+
+    public int compareTo(Card c) {
+        if (this.suit > c.suit) {
+            return 1;
+        } else if (this.suit < c.suit) {
+            return -1;
+        } else {
+            if (this.value > c.value) {
+                return 1;
+            } else if (this.value < c.value) {
+                return -1;
+            } else {
+                if (this.face.equals("10")) {
+                    if (c.face.equals("10")) return 0;
+                    return 1;
+                }
+                if (this.face.equals("J")) {
+                    if (c.face.equals("10")) return -1;
+                    if (c.face.equals("J")) return 0;
+                    return 1;
+                }
+                if (this.face.equals("Q")) {
+                    if (c.face.equals("Q")) return 0;
+                    if (c.face.equals("K")) return 1;
+                    return -1;
+                }
+                if (c.face.equals("K")) return 0;
+                return -1;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Card[" + "name=" + name + ", face=" + face + ", value=" + value +
+                    ", suit=" + suit + ']';
+    }
+}
---
+++ src/Deck.java
@@ -0,0 +1,34 @@
+public class Deck {
+    private Card[] deck;
+
+    public Deck() {
+        deck = new Card[52];
+        for (int i=0; i < 4; i++) {
+            int index = 0;
+            deck[13*i + index++] = new Card("Ace", "A", 1, i);
+            deck[13*i + index++] = new Card("Two", "2", 2, i);
+            deck[13*i + index++] = new Card("Three", "3", 3, i);
+            deck[13*i + index++] = new Card("Four", "4", 4, i);
+            deck[13*i + index++] = new Card("Five", "5", 5, i);
+            deck[13*i + index++] = new Card("Six", "6", 6, i);
+            deck[13*i + index++] = new Card("Seven", "7", 7, i);
+            deck[13*i + index++] = new Card("Eight", "8", 8, i);
+            deck[13*i + index++] = new Card("Nine", "9", 9, i);
+            deck[13*i + index++] = new Card("Ten", "10", 10, i);
+            deck[13*i + index++] = new Card("Jack", "J", 10, i);
+            deck[13*i + index++] = new Card("Queen", "Q", 10, i);
+            deck[13*i + index] = new Card("King", "K", 10, i);
+        }
+    }
+
+    public Card at(int index) {
+        return deck[index];
+    }
+
+    public void printDeck() {
+        for (Card card : deck) {
+            System.out.println(card);
+        }
+    }
+
+}
